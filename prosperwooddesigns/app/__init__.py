f750bf94dd6606e982da6551b24e068673399611
Michael Cole <mcole042891@gmail.com>
2020-08-01 17:41:02 -0500

    App automatically downloads all images from s3 bucket

--- prosperwooddesigns/app/__init__.py
+++ prosperwooddesigns/app/__init__.py
@@ -9,11 +9,12 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 from .routes import Routes
-from .extensions import Logger
+from .extensions import Logger, S3Connecter
 
 routes = Routes()
 csrf = CSRFProtect()
 logger = Logger()
+s3Conn = S3Connecter()
 
 
 def create_app():
@@ -28,6 +29,9 @@ def create_app():
                 template_folder='./templates',
                 static_folder='./static')
     app.config.from_object('config.ConfigDev')
+
+    logger.log('Importing remote image files from S3')
+    s3Conn.downloadImages()
 
     with app.app_context():
 
--- prosperwooddesigns/app/extensions.py
+++ prosperwooddesigns/app/extensions.py
@@ -1,10 +1,71 @@
+# extensions.py
+# Michael Cole
+
+# Collection of extensions to be used throughout the app
+# ------------------------------------------------------
+
 import sys
+import os
+import boto3
 
 
 class Logger:
+    '''
+    Logger object used to print to the console as well
+    as log to a file in development
+    '''
 
     def log(self, string, loc='stdout'):
+        '''
+        Prints to the console by default. Can pass a filename
+        for logs to write to
+        '''
+
         if loc == 'stdout':
             print(f'>> {string}', file=sys.stdout, flush=True)
         else:
             print(f'>> {string}', file=loc, flush=True)
+
+
+class S3Connecter:
+    '''
+    S3 Connector to be used specifically for interacting with
+    this project's S3 Bucket
+    '''
+
+    def __init__(self):
+        '''
+        Initializes by connecting to S3
+        '''
+        self.LocalImagePath = '/prosperwooddesigns/app/static/images'
+        self.S3ImageBucket = 'prosperwooddesigns'
+        self.S3ImageFolder = 'images/'
+
+        # connect to S3
+        self.s3Client = boto3.client('s3')
+        self.s3Resource = boto3.resource('s3')
+
+    def uploadImages(self):
+        '''
+        Uploads all images in the images directory of this project
+        '''
+        self.images = [f for f in os.listdir(self.LocalImagePath)]
+        for imageName in self.images:
+            self.s3Client.upload_file(
+                f'{self.LocalImagePath}/{imageName}',  # local image name
+                self.S3ImageBucket,  # bucket name
+                f'{self.S3ImageFolder}{imageName}')  # remote image name
+
+    def downloadImages(self):
+        '''
+        Downloads all images from the S3 bucket into the images directory
+        of this project
+        '''
+        bucket = self.s3Resource.Bucket(self.S3ImageBucket)
+        objects = bucket.objects.filter(Prefix=self.S3ImageFolder)
+        for obj in objects:
+            path, filename = os.path.split(obj.key)
+            if filename != '':
+                bucket.download_file(
+                    obj.key,
+                    f'{self.LocalImagePath}/{filename}')
