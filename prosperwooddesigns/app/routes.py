39e5a6b36f198e86d7a48562024faf5a32eed201
Michael Cole <mcole042891@gmail.com>
2020-09-10 22:15:09 -0500

    Dynamically presents product page based on selected item

--- prosperwooddesigns/app/routes.py
+++ prosperwooddesigns/app/routes.py
@@ -65,15 +65,19 @@ class Routes:
                                    title='Designs',
                                    joinedProductsImages=joinedProductsImages)
 
-        @app.route('/designs/product_id')
-        def designs_product():
+        @app.route('/designs/<product_name>')
+        def designs_product(product_name):
             '''
-            Routes the user to the Product Page of a chose design
-            NOTE: This is currently a WIP
+            Routes the user to the Product Page of a chosen design
             '''
-            logger.log('Serving WIP product page')
+            logger.log(f'Serving {product_name} product page')
+
+            joinedProductsImages = dbConn.getJoined_ProductImages(
+                name=product_name)
+
             return render_template('designs_product.html',
-                                   title='Product Page')
+                                   title=f'{product_name}',
+                                   joinedProductsImages=joinedProductsImages)
 
         @app.route('/requestform', methods=['GET', 'POST'])
         def requestform():
