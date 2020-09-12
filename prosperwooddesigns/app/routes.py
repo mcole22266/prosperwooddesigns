10eb6793645c08c4aa373c10d898fb70a79147ab
Michael Cole <mcole042891@gmail.com>
2020-09-12 15:34:40 -0500

    Landing page dynamically shows featured products

--- prosperwooddesigns/app/routes.py
+++ prosperwooddesigns/app/routes.py
@@ -38,9 +38,15 @@ class Routes:
             '''
             Routes the user to the Landing Page of the website
             '''
+
+            # get all featured products
+            featuredProducts = dbConn.getJoined_ProductImages(
+                featuredProducts=True)
+
             logger.log('Serving index page')
             return render_template('index.html',
-                                   title='Home')
+                                   title='Home',
+                                   featuredProducts=featuredProducts)
 
         # CURRENTLY TABLED PENDING CLIENT'S THOUGHTS
         # @app.route('/about')
